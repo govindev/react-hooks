eeccc63b28f18e0ef92eed429dccfe4e89e0033c
Vurukuti Govinda Raju <vgovindev@gmail.com>
2022-07-18 01:03:58 +0530

    usestate - initial commit

---
+++ src/components/HookCounter2.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+function HookCounter2() {
+    const initialCount = 0;
+    const [count, setCount] = useState(initialCount);
+    const incrementBy5 = () => {
+        for (let i = 0; i < 5; i++) {
+            setCount(prevCount => prevCount+1);
+        }
+    }
+    return (
+        <div>
+            Count : {count}
+            <button onClick={() => setCount(initialCount)}>Reset</button>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Increment</button>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Decrement</button>
+            <button onClick={incrementBy5}>IncrementBy5</button>
+        </div>
+    )
+}
+
+export default HookCounter2
