358d8779b30d7e4c77cf1c12343cc4433d6020d6
Vurukuti Govinda Raju <vgovindev@gmail.com>
2022-07-18 02:07:55 +0530

    useState with array

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import HookCounter from './components/HookCounter';
 import HookCounter2 from './components/HookCounter2';
 import HookCounter3 from './components/HookCounter3';
+import HookCounter4 from './components/HookCounter4';
 
 function App() {
   return (
     <div className="App">
       {/* <HookCounter/> */}
       {/* <HookCounter2/> */}
-      <HookCounter3/>
+      {/* <HookCounter3/> */}
+      <HookCounter4/>
     </div>
   );
 }
