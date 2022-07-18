03b58dc637fd12ff5ac3fdd71a1cef3867fcad4e
Vurukuti Govinda Raju <vgovindev@gmail.com>
2022-07-18 09:59:28 +0530

    useEffect hook

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HookCounter from './components/HookCounter';
 import HookCounter2 from './components/HookCounter2';
 import HookCounter3 from './components/HookCounter3';
 import HookCounter4 from './components/HookCounter4';
+import HookCounterOne from './components/HookCounterOne';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       {/* <HookCounter/> */}
       {/* <HookCounter2/> */}
       {/* <HookCounter3/> */}
-      <HookCounter4/>
+      {/* <HookCounter4/> */}
+      <HookCounterOne/>
     </div>
   );
 }
