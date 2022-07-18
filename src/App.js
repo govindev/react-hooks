565726215b6887ac660a1bc9ffa73191621b01ab
Vurukuti Govinda Raju <vgovindev@gmail.com>
2022-07-18 10:35:20 +0530

    Run effects only once

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HookCounter2 from './components/HookCounter2';
 import HookCounter3 from './components/HookCounter3';
 import HookCounter4 from './components/HookCounter4';
 import HookCounterOne from './components/HookCounterOne';
+import HookMouse from './components/HookMouse';
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       {/* <HookCounter2/> */}
       {/* <HookCounter3/> */}
       {/* <HookCounter4/> */}
-      <HookCounterOne/>
+      {/* <HookCounterOne/> */}
+      <HookMouse/>
     </div>
   );
 }
